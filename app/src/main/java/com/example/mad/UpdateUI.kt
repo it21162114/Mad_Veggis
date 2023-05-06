96cd30d6bb8ddfc876c8685fc68dc669b86b8c3b
it21162114 <99236102+it21162114@users.noreply.github.com>
2023-05-06 18:10:44 +0530

    4th commit

--- app/src/main/java/com/example/mad/UpdateUI.kt
+++ app/src/main/java/com/example/mad/UpdateUI.kt
@@ -1,11 +1,23 @@
 package com.example.mad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class UpdateUI : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
         setContentView(R.layout.activity_update_ui)
+
+        var btnNextTo = findViewById<Button>(R.id.button2)
+        btnNextTo.setOnClickListener{
+            val intent = Intent(this, Veggis::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/mad/Veggis.kt
+++ app/src/main/java/com/example/mad/Veggis.kt
@@ -1,8 +1,10 @@
 package com.example.mad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class Veggis : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,17 @@ class Veggis : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
         setContentView(R.layout.activity_veggis)
+
+        var btnNextTo = findViewById<Button>(R.id.button)
+        btnNextTo.setOnClickListener{
+            val intent = Intent(this, UpdateUI::class.java)
+            startActivity(intent)
+        }
+
+        var btnNextTo1 = findViewById<Button>(R.id.button8)
+        btnNextTo1.setOnClickListener{
+            val intent1 = Intent(this, DeleteUi::class.java)
+            startActivity(intent1)
+        }
     }
 }
